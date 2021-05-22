c9d8a6d04785e273dbb9887a32efcca3becd0cef
pmeet <pmeet007m@gmail.com>
2021-05-22 12:43:51 +0530

    Admin simple login complete

--- app/src/main/java/com/example/adminems/MainActivity.java
+++ app/src/main/java/com/example/adminems/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.adminems;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.adminems.faculty.ViewFaculty;
 import com.example.adminems.notice.DeleteNoticeActivity;
@@ -14,22 +16,38 @@ import com.google.android.material.card.MaterialCardView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     MaterialCardView uploadNotice, addGalleryImage, addEbook, addFaculty, deleteNotice;
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private Button logOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = this.getSharedPreferences("login", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        if (sharedPreferences.getString("isLogin", "false").equals("false")) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
 
         uploadNotice = findViewById(R.id.add_notification);
         addGalleryImage = findViewById(R.id.add_image);
         addEbook = findViewById(R.id.add_ebook);
         addFaculty = findViewById(R.id.add_faculty);
         deleteNotice = findViewById(R.id.delete_notice);
+        logOut = findViewById(R.id.btn_logout);
 
         uploadNotice.setOnClickListener(this);
         addGalleryImage.setOnClickListener(this);
         addEbook.setOnClickListener(this);
         addFaculty.setOnClickListener(this);
         deleteNotice.setOnClickListener(this);
+        logOut.setOnClickListener(this);
     }
 
     @Override
@@ -55,6 +73,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.delete_notice:
                 intent = new Intent(MainActivity.this, DeleteNoticeActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.btn_logout:
+                editor.putString("isLogin", "false");
+                editor.commit();
+                intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
                 break;
         }
     }
